71cb2e8aff9d58d692c5a3a3fb97d8544b44538a
Grant Raja <neko@Grants-MacBook-Pro-2.local>
2021-11-27 22:46:25 +0800

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,56 @@
+const { privateKey, bscscanApiKey } = require('./secrets.json');
+
+// import type { HardhatUserConfig } from hardhat/types;
+require("@nomiclabs/hardhat-waffle");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-etherscan");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "bsctestnet", // <-- change here for other network, default use hardhat network.
+  networks: {
+  	localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 1000000000,
+      accounts: [privateKey]
+    },
+    bsctestnet: {
+      url: "https://data-seed-prebsc-2-s3.binance.org:8545/",
+      chainId: 97,
+      accounts: [privateKey]
+    },
+  },
+  solidity: {
+    version: "0.8.2",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  etherscan: {
+    apiKey: bscscanApiKey
+  }
+};
